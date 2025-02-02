d033698b1d07706a8c4a237b65593d0e7e5f2c05
vishal chavan <vishalchavanv96@gmail.com>
2025-02-02 10:59:40 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ async function main() {
 }
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src", "views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
